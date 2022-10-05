2360b92f620538788ed6bb7db45101698463e108
Abhishek Dasgupta <abhishek.dasgupta@dtc.ox.ac.uk>
2022-10-05 11:53:35 +0100

    taxonomy: use lists instead of space separated values in enums

--- isaric/taxonomy/__init__.py
+++ isaric/taxonomy/__init__.py
@@ -3,19 +3,21 @@ from pathlib import Path
 
 from .opencodelists import OpenCodeListsEnum
 
-Sex = Enum("Sex", "male female non_binary")
+Sex = Enum("Sex", ["male", "female", "non_binary"])
 
-Pathogen = Enum("Pathogen", "COVID_19 MPXV")
+Pathogen = Enum("Pathogen", ["COVID_19", "MPXV"])
 
 Ethnicity = OpenCodeListsEnum("opensafely/ethnicity-snomed", "2020-04-27")
 
 ReferenceDateType = Enum(
-    "ReferenceDateType", "any_test_confirmation laboratory_confirmation"
+    "ReferenceDateType", ["any_test_confirmation", "laboratory_confirmation"]
 )
 
-VisitDuration = Enum("VisitDuration", "before_admission admission post_admission")
+VisitDuration = Enum(
+    "VisitDuration", ["before_admission", "admission", "post_admission"]
+)
 
-Outcome = Enum("Outcome", "death recovered")
+Outcome = Enum("Outcome", ["death", "recovered"])
 
 Observation = Enum(
     "Observation", (Path(__file__).parent / "observations.txt").read_text()
