c4d57e1aa835070f43331187b3724655f6611d90
Abhishek Dasgupta <abhishek.dasgupta@dtc.ox.ac.uk>
2022-11-23 16:54:05 +0000

    schemas: conform to PEP 484, explicit optional
    Specify optional types explicitly with '| None' as recommended
    by PEP 484.
    
    Change taxonomy/v1#Observation to taxonomy/v1#ObservationName,
    resolves conflict with table with the same name

--- isaric/schemas.py
+++ isaric/schemas.py
@@ -7,7 +7,7 @@ from .pydantic_types import IntegerRange, DateRange
 from .taxonomy import (
     Diabetes,
     Ethnicity,
-    Observation,
+    ObservationName,
     Outcome,
     Pathogen,
     PregnancyGestationalOutcome,
@@ -23,7 +23,7 @@ class Study(BaseModel):
     id: int
     name: str
     date: datetime.datetime
-    end_date: datetime.datetime = None
+    end_date: datetime.datetime | None = None
     location: dict  # GeoJSON
     country_iso3: str
     description: str
@@ -45,56 +45,56 @@ class Subject(BaseModel):
     age: int
     sex_at_birth: Sex
     ethnicity: Ethnicity
-    works_microbiology_lab: bool = None
-    works_healthcare: bool = None
+    works_microbiology_lab: bool | None = None
+    works_healthcare: bool | None = None
 
     # Pathogen
     pathogen: Pathogen
 
     # Pregnancy
-    pregnancy: bool = None
+    pregnancy: bool | None = None
     pregnancy_date_of_delivery: datetime.date
     pregnancy_birth_weight_kg: float
-    pregnancy_outcome: str = None
+    pregnancy_outcome: str | None = None
     pregnancy_gestational_outcome: PregnancyGestationalOutcome = None
-    pregnancy_whether_breastfed: bool = None
-    pregnancy_post_partum: bool = None
-    pregnancy_gestational_assessment_days: int = None
+    pregnancy_whether_breastfed: bool | None = None
+    pregnancy_post_partum: bool | None = None
+    pregnancy_gestational_assessment_days: int | None = None
 
     # Co-morbidities
-    has_chronic_hematologic_disease: bool = None
-    has_asplenia: bool = None
-    has_tuberculosis: bool = None
-    has_dementia: bool = None
-    has_obesity: bool = None
-    has_rheumatologic_disorder: bool = None
-    has_hiv: bool = None
-    has_hypertension: bool = None
-    has_malignant_neoplasm: bool = None
+    has_chronic_hematologic_disease: bool | None = None
+    has_asplenia: bool | None = None
+    has_tuberculosis: bool | None = None
+    has_dementia: bool | None = None
+    has_obesity: bool | None = None
+    has_rheumatologic_disorder: bool | None = None
+    has_hiv: bool | None = None
+    has_hypertension: bool | None = None
+    has_malignant_neoplasm: bool | None = None
     has_smoking: Smoker = None
-    has_asthma: bool = None
-    has_chronic_cardiac_disease: bool = None
-    has_chronic_kidney_disease: bool = None
-    has_diabetes: bool = None
+    has_asthma: bool | None = None
+    has_chronic_cardiac_disease: bool | None = None
+    has_chronic_kidney_disease: bool | None = None
+    has_diabetes: bool | None = None
     diabetes_type: Diabetes = None
-    has_liver_disease: bool = None
-    has_apnoea: bool = None
-    has_hiv_viral_suppression: bool = None
-    has_inflammatory_bowel_disease: bool = None
-    has_rare_disease_inborn_metabolism_error: bool = None
-    has_solid_organ_transplant: bool = None
-    has_tuberculosis_past: bool = None
-    has_hiv_art: bool = None
-    has_immunosuppression_therapy_treatment: bool = None
+    has_liver_disease: bool | None = None
+    has_apnoea: bool | None = None
+    has_hiv_viral_suppression: bool | None = None
+    has_inflammatory_bowel_disease: bool | None = None
+    has_rare_disease_inborn_metabolism_error: bool | None = None
+    has_solid_organ_transplant: bool | None = None
+    has_tuberculosis_past: bool | None = None
+    has_hiv_art: bool | None = None
+    has_immunosuppression_therapy_treatment: bool | None = None
     has_comorbidity_other: list[str] = []
 
     hiv_latest_cd4: IntegerRange
-    hiv_latest_viral_load_copies_per_ml: int = None
+    hiv_latest_viral_load_copies_per_ml: int | None = None
 
     # Outcome
-    date_death: datetime.date = None
+    date_death: datetime.date | None = None
     outcome: Outcome
-    icu_admitted: bool = None
+    icu_admitted: bool | None = None
     date_outcome: datetime.date
 
 
@@ -109,55 +109,54 @@ class Visit(BaseModel):
     end_date: datetime.date
     duration_type: VisitDuration = VisitDuration.admission
 
-    pathogen_test_date: datetime.date = None
-    icu_admission: bool = None
+    pathogen_test_date: datetime.date | None = None
+    icu_admission: bool | None = None
 
     # Multiple ICU admissions are possible
-    icu_admission_dates: list[DateRange] = None
+    icu_admission_dates: list[DateRange] | None = None
 
-    treatment_dialysis: bool = None
-    treatment_inotropes_vasopressors: bool = None
-    treatment_ecmo: bool = None
-    treatment_corticosteroid: bool = None
-    treatment_oxygen_therapy: bool = None
-    treatment_prone_position: bool = None
-    treatment_invasive_ventilation: bool = None
-    treatment_antifungal_agent: bool = None
-    treatment_antivirals: bool = None
-    treatment_antibiotics: bool = None
-    treatment_anticoagulation: bool = None
-    treatment_experimental_agent: bool = None
-    treatment_inhaled_nitric_oxide: bool = None
-    treatment_noninvasive_ventilation: bool = None
-    treatment_ace_inhibitors: bool = None
-    treatment_arb: bool = None
-    treatment_antimalarial: bool = None
-    treatment_dexamethasone: bool = None
-    treatment_nonsteroidal: bool = None
-    treatment_high_flow_nasal_cannula: bool = None
-    treatment_steroids: bool = None
-    treatment_immunosuppressant: bool = None
-    treatment_intravenous_fluids: bool = None
-    treatment_nsaid: bool = None
-    treatment_neuraminidase: bool = None
-    treatment_oxygen_interface: bool = None
-    treatment_cpr: bool = None
-    treatment_offlabel: bool = None
-    treatment_respiratory_support: bool = None
-    treatment_tocilizumab: bool = None
-    treatment_indication_anticoagulation: bool = None
-    treatment_cardiovascular: bool = None
-    treatment_colchicine: bool = None
-    treatment_immunoglobulins: bool = None
-    treatment_delirium: bool = None
-    treatment_monoclonal_antibody: bool = None
+    treatment_dialysis: bool | None = None
+    treatment_inotropes_vasopressors: bool | None = None
+    treatment_ecmo: bool | None = None
+    treatment_corticosteroid: bool | None = None
+    treatment_oxygen_therapy: bool | None = None
+    treatment_prone_position: bool | None = None
+    treatment_invasive_ventilation: bool | None = None
+    treatment_antifungal_agent: bool | None = None
+    treatment_antivirals: bool | None = None
+    treatment_antibiotics: bool | None = None
+    treatment_anticoagulation: bool | None = None
+    treatment_experimental_agent: bool | None = None
+    treatment_inhaled_nitric_oxide: bool | None = None
+    treatment_noninvasive_ventilation: bool | None = None
+    treatment_ace_inhibitors: bool | None = None
+    treatment_arb: bool | None = None
+    treatment_antimalarial: bool | None = None
+    treatment_dexamethasone: bool | None = None
+    treatment_nonsteroidal: bool | None = None
+    treatment_high_flow_nasal_cannula: bool | None = None
+    treatment_steroids: bool | None = None
+    treatment_immunosuppressant: bool | None = None
+    treatment_intravenous_fluids: bool | None = None
+    treatment_nsaid: bool | None = None
+    treatment_neuraminidase: bool | None = None
+    treatment_oxygen_interface: bool | None = None
+    treatment_cpr: bool | None = None
+    treatment_offlabel: bool | None = None
+    treatment_respiratory_support: bool | None = None
+    treatment_tocilizumab: bool | None = None
+    treatment_indication_anticoagulation: bool | None = None
+    treatment_cardiovascular: bool | None = None
+    treatment_colchicine: bool | None = None
+    treatment_immunoglobulins: bool | None = None
+    treatment_delirium: bool | None = None
+    treatment_monoclonal_antibody: bool | None = None
     treatment_other: list[str] = []
 
-    treatment_oxygen_mechanical_support: bool = None
-    treatment_oxygen_therapy: bool = None
-    treatment_pacing: bool = None
+    treatment_oxygen_mechanical_support: bool | None = None
+    treatment_pacing: bool | None = None
 
-    treatment_o2_flow_vol_max: int = None
+    treatment_o2_flow_vol_max: int | None = None
 
 
 class Observation(BaseModel):
@@ -168,8 +167,8 @@ class Observation(BaseModel):
     study_id: int
 
     date: datetime.datetime
-    end_date: datetime.datetime = None
-    value_num: float = None
-    value_char: str = None
-    is_present: bool = None
-    name: Observation
+    end_date: datetime.datetime | None = None
+    value_num: float | None = None
+    value_char: str | None = None
+    is_present: bool | None = None
+    name: ObservationName
