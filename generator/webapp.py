33420780e0dfa94c42c2d4dd545b653efbd68494
Pip Liggins <philippa.liggins@dtc.ox.ac.uk>
2023-03-06 15:37:07 +0000

    Generator: Updates to homepage Allows viewing of defined field mappings in the 'adtl' table. Allows each mapping to be edited or another added.

--- generator/webapp.py
+++ generator/webapp.py
@@ -9,6 +9,7 @@ import tomli_w
 import json
 import structures
 import re
+import pandas as pd
 
 study = {}
 subject = {}
@@ -257,6 +258,59 @@ def create_field(table, attribute, a_type):
         )
 
 
+def validate_required_fields(data):
+    # currently only validates subject and visit required fields are present
+    missing_fields_sub = []
+    for field in st.session_state.subject_required_attributes:
+        if field not in data["subject"]:
+            missing_fields_sub.append(field)
+        elif data["subject"][field] != "":
+            pass
+        elif data["subject"][field] == "":
+            missing_fields_sub.append(field)
+        elif data["subject"][field]["field"] == "":
+            missing_fields_sub.append(field)
+
+    missing_fields_visit = []
+    for field in st.session_state.visit_required_attributes:
+        if field not in data["visit"]:
+            missing_fields_visit.append(field)
+        elif data["visit"][field] != "":
+            pass
+        elif data["visit"][field] == "":
+            missing_fields_visit.append(field)
+        elif data["visit"][field]["field"] == "":
+            missing_fields_visit.append(field)
+
+    if len(missing_fields_sub + missing_fields_visit) > 0:
+        st.error(
+            f"There are required fields missing from the subject or visit tables.\n\
+                 Subject: {missing_fields_sub}\n\
+                 Visit: {missing_fields_visit}"
+        )
+        return False
+    return True
+
+
+def generate_parser(data):
+    if validate_required_fields(data):
+        data["subject"]["subject_id"]["sensitive"] = True
+        data["visit"]["subject_id"]["sensitive"] = True
+        data["visit"]["visit_id"]["sensitive"] = True
+
+        for obs in st.session_state.obs_list:
+            if "observation" in data.keys():
+                data["observation"].append(obs)
+            else:
+                data["observation"] = [obs]
+
+        with open(f"generator/{parser_name}-generator.toml", "wb") as f:
+            tomli_w.dump(data, f)
+
+        return True
+    return False
+
+
 ### -------------------------------------------------------------------------------------
 # START STREAMLIT -----------------------------------------------------------------------
 ### -------------------------------------------------------------------------------------
@@ -319,7 +373,6 @@ def get_attributes_types(table: str):
     st.session_state.obs_required_attributes,
     st.session_state.obs_attr_types,
 ) = get_attributes_types("observation")
-# observation, obs_attributes, obs_required_attributes, obs_attr_types = get_attributes_types('observation') # TODO
 
 if "toml_dict" not in st.session_state:
     st.session_state.toml_dict = {}
@@ -327,13 +380,13 @@ if "toml_dict" not in st.session_state:
     with open("generator/base-parser.toml", "rb") as f:
         st.session_state.toml_dict = tomli.load(f)
 
+st.header("Study information")
 col1, col2 = st.columns(2)
-
 with col1:
-    st.header("Study information")
     parser_name = st.text_input("Parser name:", value="isaric-ccpuk")
     parser_descrip = st.text_input("Parser description", value="ISARIC CCPUK study")
     parser_country = st.text_input("Country code:", value="GBR")
+with col2:
     study_date = st.text_input("Study date:", value="2021-01-01")
     pathogen = st.text_input("Pathogen under study:", value="COVID-19")
 
@@ -350,130 +403,118 @@ st.session_state.toml_dict["subject"]["pathogen"] = pathogen
 
 st.session_state.toml_dict["visit"]["country_iso3"] = parser_country
 
-with col2:
-    st.header("Common mapping types")
-    st.write(
-        "Use this section to describe common field mappings - e.g., how the form handles yes/no/not known responses."
-    )
-
-    # TODO: Date heirarchies for the Observation table aren't included here...
-
-    no_fields = col2.number_input(
-        "Number of field mappings to save:",
-        value=2,
-        key="fieldmapping_numbers",
-    )
-
-    refs = []
-    map_keys = []
-    examples = [
-        ["Y/N/NK", "1=true, 2=false, 3=false"],
-        [],
-        [
-            "ethnicity",
-            "1=White, 2=Arab, 3=Black, 4=East Asian, 5=South Asian, 6=West Asian, 7=Latin American",
-        ],
-        [],
-    ]
-
-    mf_grid = make_grid(no_fields * 2, 2)
-
-    for i in range(0, no_fields * 2, 2):
-        with mf_grid[i][0]:
-            refs.append(
-                st.text_input(
-                    "Mapping reference",
-                    value=examples[i][0] if i < len(examples) else "",
-                    key="mappingkeyref" + str(i),
-                )
-            )
-        with mf_grid[i][1]:
-            map_keys.append(
-                st.text_input(
-                    "value pairs",
-                    value=examples[i][1] if i < len(examples) else "",
-                    key="mappingkey" + str(i),
-                )
-            )
-
-        for square in [
-            mf_grid[i + 1][0],
-            mf_grid[i + 1][1],
-        ]:
-            square.markdown("#")
-
-    for r, mk in zip(refs, map_keys):
-        st.session_state.toml_dict["adtl"]["defs"][
-            r
-        ] = structures.predefined_value_maps(string_to_dict(mk))
 
 st.markdown("#")
 
+st.header(
+    "Below are the common mapping types for value fields currently defined in the parser file."
+)
+st.write(
+    "To edit the existing maps, or add a new one, use the form to the right. If a reference which already exists,\
+          e.g. 'Y/N/NK' is given as the reference in the form, the current map will be overwritten."
+)
 
-def validate_required_fields(data):
-    # currently only validates subject and visit required fields are present
-    missing_fields_sub = []
-    for field in st.session_state.subject_required_attributes:
-        if field not in data["subject"]:
-            missing_fields_sub.append(field)
-        elif data["subject"][field] != "":
-            pass
-        elif data["subject"][field] == "":
-            missing_fields_sub.append(field)
-        elif data["subject"][field]["field"] == "":
-            missing_fields_sub.append(field)
+col1, col2 = st.columns(2)
 
-    missing_fields_visit = []
-    for field in st.session_state.visit_required_attributes:
-        if field not in data["visit"]:
-            missing_fields_visit.append(field)
-        elif data["visit"][field] != "":
-            pass
-        elif data["visit"][field] == "":
-            missing_fields_visit.append(field)
-        elif data["visit"][field]["field"] == "":
-            missing_fields_visit.append(field)
+with col1:
+    # show a json file for the current mappings
+    st.json(st.session_state.toml_dict["adtl"]["defs"])
 
-    if len(missing_fields_sub + missing_fields_visit) > 0:
-        st.error(
-            f"There are required fields missing from the subject or visit tables.\n\
-                 Subject: {missing_fields_sub}\n\
-                 Visit: {missing_fields_visit}"
+with col2:
+    st.markdown("### Add or edit a reference map:")
+    mapping_ref = st.text_input("Mapping reference", placeholder="Y/N/NK")
+
+    multifield_mapping = st.checkbox("Are there multiple fields to combine?")
+    if multifield_mapping:
+        subcol1, subcol2 = st.columns(2)
+        combination_type = subcol1.selectbox(
+            "Which combined type should be applied?",
+            ["any", "all", "firstNonNull", "list"],
+            key="MappingscombinationType",
         )
-        return False
-    return True
+        no_fields = subcol2.number_input(
+            "How many fields are there to combine?", value=2
+        )
+        fields = []
 
+        st.markdown("#### Fields to combine:")
 
-def generate_parser(data):
-    if validate_required_fields(data):
-        data["subject"]["subject_id"]["sensitive"] = True
-        data["visit"]["subject_id"]["sensitive"] = True
-        data["visit"]["visit_id"]["sensitive"] = True
+        mf_grid = make_grid(no_fields * 2, 2)
 
-        for obs in st.session_state.obs_list:
-            if "observation" in data.keys():
-                data["observation"].append(obs)
+        for i in range(0, no_fields * 2, 2):
+            cell1, cell2 = mf_grid[i][0], mf_grid[i][1]
+            with cell1:
+                field_name = st.text_input(
+                    f"Field name ({i})", key="keymaps_fields" + str(i)
+                )
+                st.markdown("#")
+                st.markdown("###")
+                descrip = st.text_input(
+                    "Description (optional)", key="keymaps_description" + str(i)
+                )
+            with cell2:
+                val_map = st.text_area(
+                    "Value map (optional)",
+                    key="keymaps_values" + str(i),
+                    placeholder="2=slight, 3=moderate, 4=severe, 5=unable",
+                )
+                conditional = st.text_input(
+                    "Condition (optional)",
+                    key="keymaps_condition" + str(i),
+                    placeholder="dsterm=4",
+                )
+            if conditional != "":
+                if val_map != "":
+                    fields.append(
+                        structures.conditional_field(
+                            field_name,
+                            descrip,
+                            "if",
+                            string_to_dict(conditional, conditional=True),
+                            values=string_to_dict(val_map),
+                        )
+                    )
+                else:
+                    print(string_to_dict(conditional, conditional=True))
+                    fields.append(
+                        structures.conditional_field(
+                            field_name,
+                            descrip,
+                            "if",
+                            string_to_dict(conditional, conditional=True),
+                        )
+                    )
+            elif val_map != "":
+                fields.append(
+                    structures.field_value_mapped(
+                        field_name,
+                        descrip,
+                        values=string_to_dict(val_map),
+                    )
+                )
             else:
-                data["observation"] = [obs]
+                fields.append(structures.single_field(field_name, descrip))
 
-        with open(f"generator/{parser_name}-generator.toml", "wb") as f:
-            tomli_w.dump(data, f)
+            for square in [
+                mf_grid[i + 1][0],
+                mf_grid[i + 1][1],
+            ]:
+                square.markdown("#")
+                square.markdown("#")
+    else:
+        value_field = st.text_area(
+            "Value map",
+            key="keymaps_values",
+            placeholder="2=slight, 3=moderate, 4=severe, 5=unable",
+        )
 
-        return True
-    return False
-
-
-_, col2, col3, _ = st.columns(4)
-
-if col2.button(
-    "Generate Parser", type="primary", use_container_width=True, key="parsergen-home"
-):
-    if generate_parser(st.session_state.toml_dict):
-        col2.write("Parser generated! Available in the 'generator' folder.")
-        with open(f"generator/{parser_name}-generator.toml", "rb") as f:
-            col3.download_button(
-                label="Download Parser",
-                data=f,
-                file_name=f"{parser_name}.toml",
-                use_container_width=True,
-            )
+    if st.button("Upload edits to reference maps", type="primary"):
+        if multifield_mapping:
+            st.session_state.toml_dict["adtl"]["defs"][
+                mapping_ref
+            ] = structures.combined_type(combination_type, "", fields)
+        else:
+            st.session_state.toml_dict["adtl"]["defs"][mapping_ref] = {
+                "values": string_to_dict(value_field)
+            }
+        st.experimental_rerun()
