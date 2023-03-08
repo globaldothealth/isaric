2b8704aec4d271cde5ec150dd9607eb945ad0642
Pip Liggins <philippa.liggins@dtc.ox.ac.uk>
2023-03-08 15:57:22 +0000

    Updated folder structure Allows functions to be passed into rather than between webpages (numerical values in names means imports from files in 'pages' doesn't work). Single field generator works for both subject/visit and observations.

---
+++ generator/forms/observation.py
@@ -0,0 +1,140 @@
+import streamlit as st
+import forms.structures as structures
+
+from forms.structures import string_to_dict
+
+
+def observation_form(button_label):
+    with st.form(key="observation", clear_on_submit=True):
+        st.write("Required fields:")
+        (
+            col1,
+            col2,
+        ) = st.columns(2)
+        with col1:
+            name = st.selectbox(
+                "Observation name", st.session_state.obs["properties"]["name"]["enum"]
+            )
+            phase = st.radio(
+                "Observation phase", ["admission", "study", "followup"], horizontal=True
+            )
+        with col2:
+            date = st.text_input(
+                "Date of observation",
+                value="admissionDateHierarchy",
+                help="This can either be a single field name where the date is stored,\
+                                or a reference to a pre-defined date hierarchy.",
+            )
+            condition = st.text_input(
+                "Condition required to show this observation as a row, e.g. 'cmyn=1'"
+            )
+
+        st.markdown("#")
+        st.write("Optional fields")
+
+        (
+            col1,
+            col2,
+        ) = st.columns(2)
+
+        with col1:
+            st.write(
+                "**is_present**: Records if the observation denotes presence (*true*) or absence (*false*) of the symptom."
+            )
+            subcol1, subcol2 = st.columns(2)
+            is_present_field = subcol1.text_input("Field name", key="ispresent_field")
+            is_present_values = subcol2.text_input("Value map", key="ispresent_value")
+            st.write("Values: Records the value of **numerical observations**")
+            value_field = st.text_input(
+                "Field name",
+                help="For numerical values - E.g., 'diabp_vsorres' storing the recorded blood pressure for the observation.",
+                key="value_field",
+            )
+            subcol1, subcol2, subcol3 = st.columns(3)
+            value_units = subcol1.text_input(
+                "Desired unit for value (optional)",
+                placeholder="kg",
+                help="Desired unit for the value field, e.g. kg for mass, '°C' for temperature, years for age.",
+                key="value_units_required",
+            )
+            value_sunit_field = subcol2.text_input(
+                "Unit source field (optional)",
+                help="Field name where the unit of the measurement taken is recorded",
+                key="value_units_source",
+            )
+            value_sunits_map = subcol3.text_input(
+                "Unit field mapping (optional)",
+                placeholder="1=kg, 2=lbs",
+                help="",
+                key="value_units_map",
+            )
+            st.write("Text: records value map for **text-based observations**")
+            subcol1, subcol2 = st.columns(2)
+            text_field = subcol1.text_input("Field name", key="text_field")
+            text_values = subcol2.text_input(
+                "Value map",
+                help="For text observations - E.g., for a conciousness scale, field = 'avpu_vsorres', values = '1=Alert, 2=Verbal, 3=Pain, 4=Unresponsive' }.",
+                key="text_value",
+            )
+        with col2:
+            st.write(
+                "**Occurance period**: ISO 8601 duration string referring to the time period before the date in which observation occurred"
+            )
+            occurence_period = st.text_input("Field name")
+            st.write("**Context**: Context that qualifies the observation")
+            context = st.text_input(
+                "Field name",
+                help="e.g. *axillary* temperature, or *room air* oxygen saturation measurement",
+            )
+
+            st.write("")
+            st.write("")
+            subcol1, subcol2, subcol3 = st.columns([1, 4, 1])
+            add_observation = subcol2.form_submit_button(label=button_label)
+        if add_observation:
+            # check for potential dictionary entries
+            try:
+                is_present_values = string_to_dict(is_present_values)
+            except:
+                pass
+
+            try:
+                text_values = string_to_dict(text_values)
+            except:
+                pass
+
+            if value_units != "":
+                value = structures.field_with_unit(
+                    value_field,
+                    "",
+                    value_units,
+                    value_sunit_field,
+                    string_to_dict(value_sunits_map),
+                )
+            else:
+                value = value_field
+
+            # write to a data store
+            observation = {
+                "name": name,
+                "date": date,
+                "phase": phase,
+                "is_present": structures.field_value_mapped(
+                    is_present_field, "", is_present_values
+                ),
+                "if": string_to_dict(condition, conditional=True),
+                "value": value,
+                "text": structures.field_value_mapped(text_field, "", text_values),
+                "occurence_period": occurence_period,
+                "context": context,
+            }
+
+            optional = ["is_present", "value", "text", "occurence_period", "context"]
+            for f in optional:
+                if observation[f] == "":
+                    del observation[f]
+                elif type(observation[f]) == dict:
+                    if observation[f]["field"] == "":
+                        del observation[f]
+
+            return observation
--- generator/pages/4-single_field_generator.py
+++ generator/pages/4-single_field_generator.py
@@ -1,11 +1,14 @@
 # single_field_generator.py
-# This page will print the toml code for a single field, in the correct format to be read by adtl.
+# This page will output the toml code for a single field, in the correct format to be read by adtl.
 
 import streamlit as st
-import structures
 import tomli_w
 
-from webapp import create_field, generate_parser, string_to_dict
+from forms.observation import observation_form
+from forms.visit_subject import create_field
+
+if "single_field_observation" not in st.session_state:
+    st.session_state.single_field_observation = {}
 
 st.header("Generate a single parser attribute")
 st.write(
@@ -13,22 +16,31 @@ st.write(
           to copy into a parser you are working on."
 )
 
-table = st.text_input("Table", value="subject")
-attribute = st.selectbox(
-    "Attribute name",
-    st.session_state.subject_attributes
-    + st.session_state.visit_attributes
-    + st.session_state.obs_attributes,
-    help="Every attribute option for the subject, visit and obs table is listed here; start typing your attribute name to seach through the list.",
-)
+table = st.selectbox("Table", ["subject", "visit", "observation"])
 
-# Figure out a way to change the layout here rather than the somewhat restrictive 4 columns; might need to re-do the create_field function for this section
-# - but can still reuse a lot of the code.
-result = create_field(table, attribute, "")
+if table == "observation":
+    obs = observation_form("Click here to compile the observation")
+    if obs is not None:
+        st.session_state.single_field_observation = obs
+else:
+    attribute = st.selectbox(
+        "Attribute name",
+        st.session_state.subject_attributes
+        + st.session_state.visit_attributes
+        + st.session_state.obs_attributes,
+        help="Every attribute option for the subject, visit and obs table is listed here; start typing your attribute name to seach through the list.",
+    )
+    result = create_field(table, attribute, "", [0.1, 1, 1, 1])
 
 if st.button("Generate toml snippet", type="primary"):
-    toml_snippet = {table: {attribute: result}}
+    if table == "observation":
+        toml_snippet = {table: [st.session_state.single_field_observation]}
+    else:
+        toml_snippet = {table: {attribute: result}}
 
     toml_string = tomli_w.dumps(toml_snippet)
+
+    with open("generator/test_dump.toml", "wb") as f:
+        tomli_w.dump(toml_snippet, f)
 
     st.code(toml_string, language="toml")
